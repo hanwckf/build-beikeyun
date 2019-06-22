f10d63612fd14f7a8d9b452a385e4cebca8a6a13
hanwckf <my375229675@gmail.com>
2019-06-22 21:55:04 +0800

    alpine: simplify script

--- tools/alpine/init.sh
+++ tools/alpine/init.sh
@@ -2,31 +2,26 @@
 
 export PATH=/usr/sbin:/usr/bin:/bin:/sbin
 
+add_svc(){
+	runlevel="$1"
+	svcs="$2"
+	for svc in ${svcs}; do
+		if [ -f ./etc/init.d/${svc} ]; then
+			ln -sf /etc/init.d/${svc} ./etc/runlevels/${runlevel}/${svc}
+		fi
+	done
+}
+
 apk update --no-progress && \
 	apk add --no-progress alpine-base haveged dropbear parted e2fsprogs-extra dropbear-scp tzdata
 
 echo "root:admin" | chpasswd
 
-svc_boot="networking urandom swclock sysctl modules"
-for svc in $svc_boot; do
-	if [ -f ./etc/init.d/$svc ]; then
-		ln -sf /etc/init.d/$svc ./etc/runlevels/boot/$svc
-	fi
-done
+add_svc "boot" "networking urandom swclock sysctl modules"
 
-svc_default="crond dropbear haveged ntpd"
-for svc in $svc_default; do
-	if [ -f ./etc/init.d/$svc ]; then
-		ln -sf /etc/init.d/$svc ./etc/runlevels/default/$svc
-	fi
-done
+add_svc "default" "crond dropbear haveged ntpd"
 
-svc_shutdown="killprocs mount-ro savecache"
-for svc in $svc_shutdown; do
-        if [ -f ./etc/init.d/$svc ]; then
-                ln -sf /etc/init.d/$svc ./etc/runlevels/shutdown/$svc
-        fi
-done
+add_svc "shutdown" "killprocs mount-ro savecache"
 
 sed -i '/^tty[2-6]/d' ./etc/inittab
 
